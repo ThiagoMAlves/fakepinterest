36c4cc7a132ef4c9c485dc08d4ea9d5a9ce78648
Thiago Marinho <thiagomarinhohu3@gmail.com>
2025-12-12 18:15:11 -0300

    Commit Inicial

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SECRET_KEY'] = '0x7a4d8b2c9f1e3d5a3b6c4e8g1a13a4b5c6d7e8f9a0b'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
+
+from fakepinterest import routes
